261a6fb5b546d60295a1351c203cd8f24925d408
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2024-02-12 23:58:19 -0500

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
   return (
+    <div className="footer">
     <footer className="SubNav">
         <div className="icons">
             <SocialIcon url="https://www.linkedin.com/in/sarah-smallwood565/" style={{ height: 60, width: 60 }}/>
@@ -12,8 +13,19 @@ function Footer() {
             <SocialIcon url="https://www.shutterstock.com/g/Redhead_ATW?_gl=1*1lym7eq*_ga*MTkwNjk3NzgzOC4xNjk3MjU1MDY1*_ga_5JRYE4Y8J9*MTcwNDk0MDExMi44LjEuMTcwNDk0MDEzMi40MC4wLjA.*_fplc*RnFvSlo4Sm0lMkZleWt0Sm9JbFQ5cWt0ODI0UThrek9qYktKVHdIT04wR3h1SjNQQm1qTHdwV3IyTlQlMkY5WlVYTlRldEJjQ01HQWk2NkpidVpFVSUyRmQwaSUyRnJTMkxEUVh0c1RhOW81bUpNQWRkaWRxa0JvZnEyN0N6VzZESyUyQnhzQSUzRCUzRA.." style={{ height: 60, width: 60, backgroundcolor: 'whitesmoke' }}/>
             <SocialIcon url="https://github.com/SarahSmallwood" style={{ height: 60, width: 60 }}/>
         </div>
-
     </footer>
+    <div className='bottomRow'>
+      <div className='location'>
+        <img className='locationPin' alt='location pin logo' src='https://png.pngtree.com/png-vector/20190420/ourmid/pngtree-location-vector-icon-png-image_963325.jpg' />
+        <div className='locationName'>SCOTTSDALE</div>
+      </div>
+      <div className='email'>
+        <img className='emailLogo' alt='white envelope email logo' src='https://i.pinimg.com/originals/b2/a3/fb/b2a3fbd8bd83857c74ff274ecd79c40f.png' />
+        <a className='emailName' href="sarahsmallwood56@gmail.com"> sarahsmallwood56@gmail.com</a>
+      </div>
+      <div className='nameLogo'></div>
+    </div>
+    </div>
   )
 }
 
